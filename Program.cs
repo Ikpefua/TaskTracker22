60b18740ca2f6ae08e7b9867809275f54b0f7b40
Ikpefua <124126187+Ikpefua@users.noreply.github.com>
2023-08-11 03:48:33 +0100

    Update Program.cs
    The programmers Object and property function was implemented

--- Program.cs
+++ Program.cs
@@ -32,6 +32,103 @@ namespace TaskLoader
         Recuring
     }
 
+    internal class Program 
+    {
+        static List<Programmer> programmers = new List<Programmer>();
+
+        static void Main(string[] args) 
+        {
+            bool exit = false;
+            while(!exit) 
+            {
+                Console.WriteLine("1. Add Programmer");
+                Console.WriteLine("2. Add Task");
+                Console.WriteLine("3. View Programmer's Tasks");
+                Console.WriteLine("4. Update Task Status");
+                Console.WriteLine("5. View All Tasks");
+                Console.WriteLine("6. View All Programmers");
+                Console.WriteLine("7. Exit");
+                Console.Write("Select an option: ");
+                
+                //Switch Statement for choice.Added task, is the choice
+                int choice = int.Parse(Console.ReadLine());
+                switch(choice) 
+                {
+                    case 1:
+                        AddProgrammer();
+                        break;
+                    case 6:
+                        ViewProgrammers();
+                        break;
+                    case 7:
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input, Please try again");
+                }
+                
+            }
+        }
+        
+        private static void ViewProgrammer()
+        {
+            if(programmers.Count < 1)
+            {
+                Console.WriteLine("List is Empty");
+            } else 
+            {
+                Console.WriteLine("List Of Programmers Include ");
+                Console.WriteLine("_____________________________________________________");
+                Console.WriteLine();
+                
+                foreach(var item in programmers) 
+                {
+                    Console.WriteLine("Id: " + item.Id "\nName: " + item.Name "\nRole: " + item.Role "\nLevel: " + item.Level;
+                }
+                Console.WriteLine("________________________________________________________");
+                Console.WriteLine();
+            }
+        }
+     
+        
+        private static void AddProgrammer() 
+        {
+            //Collect user input
+            Console.Write("Enter Programmer Name: ");
+            string name = Console.ReadLine();
+            Console.WritLine("-------------------------------");
+            Console.WriteLine();
+            Consosle.Write("Role: ");
+            string role = Console.ReadLine();
+            Console.WritLine("------------------------------");
+            Console.WriteLine();
+            Console.Write("Level: ")
+            string level = Console.ReadLine();
+            Console.WritLine("----------------------------");
+            Console.WriteLine();
+
+            int id = programmers.Count + 1;
+
+            // Programmer list;
+            var programmList = new Programmer
+            {
+                Console.WritLine("----------------------------------------------");
+                Id = id,
+                Name = name,
+                Role = role,
+                Level = level
+                Console.WritLine("----------------------------------------------");
+                Console.WritLine();
+            }
+            programmers.Add(programList)
+            Console.WriteLine("Programmer Added Successfully");
+            Console.WriteLine("________________________________________________________");
+            Console.WriteLine();
+        }
+
+        
+    }
+
     internal class ProgramTask
     {
         static List<Tasks> tasks = new List<Tasks>();
@@ -76,7 +173,8 @@ namespace TaskLoader
             else
             {
                 Console.WriteLine("List of Task is ");
-                Console.WriteLine("________________________________________________________"); Console.WriteLine();
+                Console.WriteLine("________________________________________________________"); 
+                Console.WriteLine();
                 foreach (var item in tasks)
                 {
                     Console.WriteLine("TaskId : " + item.TaskId + " TaskName : " + item.TaskName + " comment: " + item.Comment, item.ProgrammerId);
